2497c4a6465ab2cef43e0743dc05b17ca4e0c2a2
AdiyanRahman <adiyanrahman.compsci@gmail.com>
2025-10-02 10:17:33 -0500

    Delete SlotMachine.java

--- SlotMachine.java
+++
@@ -1,9 +0,0 @@
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-
-public class SlotMachine extends Assets{
-    public SlotMachine() throws IOException {
-        super(6, 120, 0, ImageIO.read(new File("slot_machine.png")));
-    }
-}
