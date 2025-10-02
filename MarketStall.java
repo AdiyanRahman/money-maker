4ed0171b00f7a9772609dff5215ce2b07bc68ff7
AdiyanRahman <adiyanrahman.compsci@gmail.com>
2025-10-02 10:17:19 -0500

    Delete MarketStall.java

--- MarketStall.java
+++
@@ -1,9 +0,0 @@
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-
-public class MarketStall extends Assets{
-    public MarketStall() throws IOException {
-        super(2, 40, 0, ImageIO.read(new File("marketStall.png")));
-    }
-}
