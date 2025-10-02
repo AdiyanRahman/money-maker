ae4f2a498670a3609420e39b730af4317ff70883
AdiyanRahman <adiyanrahman.compsci@gmail.com>
2025-10-02 10:16:03 -0500

    Delete Bank.java

--- Bank.java
+++
@@ -1,9 +0,0 @@
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-
-public class Bank extends Assets{
-    public Bank() throws IOException {
-        super(5, 100, 0, ImageIO.read(new File("bank.png")));
-    }
-}
