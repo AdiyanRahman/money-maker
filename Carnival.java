035180df42e05bc28770dd00ff4801e2446fe259
AdiyanRahman <adiyanrahman.compsci@gmail.com>
2025-10-02 10:16:12 -0500

    Delete Carnival.java

--- Carnival.java
+++
@@ -1,9 +0,0 @@
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-
-public class Carnival extends Assets{
-    public Carnival() throws IOException {
-        super(7, 140, 0, ImageIO.read(new File("icon.png")));
-    }
-}
