7e8b346ad6cd17e8696184727102df2a8c344cd8
AdiyanRahman <adiyanrahman.compsci@gmail.com>
2025-10-02 10:16:27 -0500

    Delete Farm.java

--- Farm.java
+++
@@ -1,9 +0,0 @@
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-
-public class Farm extends Assets{
-    public Farm() throws IOException {
-        super(3, 60, 0, ImageIO.read(new File("farm.png")));
-    }
-}
