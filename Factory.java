90925bcce63952e1242559dcd049bf7125a61f4e
AdiyanRahman <adiyanrahman.compsci@gmail.com>
2025-10-02 10:16:19 -0500

    Delete Factory.java

--- Factory.java
+++
@@ -1,9 +0,0 @@
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-
-public class Factory extends Assets{
-    public Factory() throws IOException {
-        super(4, 80, 0, ImageIO.read(new File("factory.png")));
-    }
-}
